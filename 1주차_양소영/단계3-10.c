c4440dffcee5bf0f11363de44e9ec45d5990ff5c
YANG-SO-YEONG <77045346+YANG-SO-YEONG@users.noreply.github.com>
2021-01-09 15:09:15 +0900

    Update 단계3-10.c

--- 1주차_양소영/단계3-10.c
+++ 1주차_양소영/단계3-10.c
@@ -4,11 +4,11 @@
 void main() {
 	int N;
 	scanf(" %d", &N);
-	for (int i = 0; i < N; i++) { //¸î ¹øÂ° ÁÙ?
+	for (int i = 0; i < N; i++) { 
 		for (int j = 0;j <= (N-i); j++) {
 			printf(" ");
 		}
-		for (int j = 0; j <= i; j++) { //¸î °³ Ãâ·Â?
+		for (int j = 0; j <= i; j++) { 
 			printf("*");
 		}
 		printf("\n");
