47f8b843809ba7a7b521e4e1d8076bbe624c95d6
YANG-SO-YEONG <77045346+YANG-SO-YEONG@users.noreply.github.com>
2021-01-23 14:06:55 +0900

    Add files via upload

---
+++ 8-5. 10250 ACM호텔.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+void main() {
+	int W, H, N, room, floor, num = 1; //room은 호실, floor는 층
+	scanf_s(" %d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf_s("%d %d %d", &H, &W, &N);
+		floor = N % H; //층수는 N % H
+		room = (N / H) + 1; //호실은 ( N / H )+1
+		printf("%d%02d\n", floor, room);
+	}
+}*/
