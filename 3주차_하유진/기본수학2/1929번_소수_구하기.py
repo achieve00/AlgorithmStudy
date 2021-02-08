b7585bb199fd126b79b03ad039f94a6da0d7d395
Yujin Ha <76898768+Yujin5077@users.noreply.github.com>
2021-02-08 20:35:22 +0900

    Add files via upload

---
+++ 3주차_하유진/기본수학2/1929번_소수_구하기.py
@@ -0,0 +1,11 @@
+m, n = map(int, input().split())
+
+def solution(m, n):
+    for i in range(m, n+1):
+        count = 0
+    for j in range(1, i + 1):
+        if i % j == 0:
+            count += 1
+
+    if count == 2:
+        print(i)
